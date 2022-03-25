b4410ebf7216f3904e96b1eee004cbca642b0b38
lqewo <vrkonosha@gmail.com>
2022-03-25 22:43:38 +0300

    added desctop version pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,26 @@
 import * as flsFunction from "./modules/functions.js";
 
+
 flsFunction.isWebp();
 
- import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-  const swiper = new Swiper();
+Swiper.use(Navigation, Pagination);
+
+const swiper = new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
+
+  $(function()  {
+    $('.tab__element').on('click', function (e){
+        e.preventDefault()
+        $('.tab__element').removeClass('tab__element--active')
+        $(this).addClass('tab__element--active')
+
+        $('.catalog-slider').removeClass('catalog-slider--active')
+        $($(this).attr('href')).addClass('catalog-slider--active')
+    })
+  });
